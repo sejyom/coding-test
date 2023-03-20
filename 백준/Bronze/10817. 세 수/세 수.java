6b7c9bec984b1ff7e10f2673c286f5a5ef0c6fe4
세정 <103199551+sejyom@users.noreply.github.com>
2023-03-20 21:18:57 +0900

    [Bronze III] Title: 세 수, Time: 132 ms, Memory: 14524 KB -BaekjoonHub

---
+++ 백준/Bronze/10817. 세 수/세 수.java
@@ -0,0 +1,12 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int[] arr = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        Arrays.sort(arr);
+        System.out.println(arr[1]);
+    }
+}
