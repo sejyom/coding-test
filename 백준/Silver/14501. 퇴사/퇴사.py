29ff2c19dc9783fb928df1d2aaf645bfee57a5cc
세정 <103199551+sejyom@users.noreply.github.com>
2023-04-10 02:33:56 +0900

    [Silver III] Title: 퇴사, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/14501. 퇴사/퇴사.py
@@ -0,0 +1,17 @@
+import sys
+
+n = int(sys.stdin.readline())
+lst = [[0, 0]]
+dp = [0 for i in range(n+1)]
+
+for i in range(n):
+    t, p = map(int, sys.stdin.readline().split(" "))
+    lst.append([t, p])
+
+for i in range(n-1, -1, -1):
+    if i + lst[i+1][0] <= n: # 상담 시작일 + 걸리는 시간이 퇴사일을 넘기지 않을 때
+        dp[i] = max(dp[i+1], lst[i+1][1]+dp[i+lst[i+1][0]])
+    else:
+        dp[i] = dp[i+1]
+
+print(dp[0])
