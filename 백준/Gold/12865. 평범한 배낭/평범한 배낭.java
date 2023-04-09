bd54ad7749ca73fd15d5202990821bb69bd198fc
세정 <103199551+sejyom@users.noreply.github.com>
2023-04-09 18:59:54 +0900

    [Gold V] Title: 평범한 배낭, Time: 304 ms, Memory: 57008 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -14,18 +14,12 @@ public class Main {
             w[i] = scanner.nextInt();
             v[i] = scanner.nextInt();
         }
-
-        for (int i = 1; i <= n; i++) {
-            if (i <= n)
-                dp[0][i] = 0;
-            dp[i][0] = 0;
-        }
-
+        
         for (int i = 1; i < n + 1; i++) {
             for (int j = 1; j < k + 1; j++) {
-                if (w[i] <= j)
+                if (w[i] <= j) // 물건의 무게가 j(현 최대 배낭 무게)를 초과하지 않을 때 -> 물건을 넣을 수 있음
                     dp[i][j] = Math.max(dp[i-1][j], v[i]+dp[i-1][j-w[i]]);
-                else // w[i] > j
+                else // w[i] > j 물건의 무게가 j(현 최대 배낭 무게)를 초과할 때 -> 물건을 넣을 수 없으므로 이전 값을 그대로 적용
                     dp[i][j]=dp[i-1][j];
                 max = Math.max(dp[i][j], max);
             }
