a5e73752ffc60930381898e95202a1d395576cde
세정 <103199551+sejyom@users.noreply.github.com>
2023-04-12 14:04:20 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 120 ms, Memory: 14124 KB -BaekjoonHub

---
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++)
+            arr[i] = numOfCases(Integer.parseInt(br.readLine()));
+
+        for (int i : arr)
+            System.out.println(i);
+    }
+
+    public static int numOfCases(int num) {
+        int[] arr = {0, 1, 2, 4}; //0:0, 1:1, 2:2, 3:4
+
+        if (num==3)
+            return 4;
+        if (num==2)
+            return 2;
+        if (num==1)
+            return 1;
+
+        return numOfCases(num - 1) + numOfCases(num - 2) + numOfCases(num - 3);
+    }
+}
