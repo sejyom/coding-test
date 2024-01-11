163ae7afe46ac5fb30966fe75aea52c845410ceb
세정 <103199551+sejyom@users.noreply.github.com>
2024-01-11 15:18:20 +0900

    [level 1] Title: 폰켓몬, Time: 5.87 ms, Memory: 91.8 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -2,7 +2,7 @@ import java.util.Arrays;
 
 class Solution {
     public int solution(int[] nums) {
-        int[] arr = Arrays.stream(nums).sorted().distinct().toArray();
+        int[] arr = Arrays.stream(nums).distinct().toArray();
         return arr.length >= nums.length / 2 ? nums.length / 2 : arr.length;
     }
 }
