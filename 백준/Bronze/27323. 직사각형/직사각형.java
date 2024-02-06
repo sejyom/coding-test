bcf543b927262c9c869df3464e858f854b7c53d2
세정 <103199551+sejyom@users.noreply.github.com>
2024-02-06 10:05:08 +0900

    [Bronze V] Title: 직사각형, Time: 120 ms, Memory: 14104 KB -BaekjoonHub

---
+++ 백준/Bronze/27323. 직사각형/직사각형.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int a = Integer.parseInt(br.readLine());
+        int b = Integer.parseInt(br.readLine());
+
+        System.out.println(calculateRectangleArea(a, b));
+    }
+
+    public static int calculateRectangleArea(int a, int b) {
+        return a*b;
+    }
+}
