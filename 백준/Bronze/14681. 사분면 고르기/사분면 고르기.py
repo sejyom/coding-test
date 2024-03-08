eae46b2878aefd5fb1e360eebffa405ebecadd31
세정 <103199551+sejyom@users.noreply.github.com>
2024-03-08 10:46:41 +0900

    [Bronze V] Title: 사분면 고르기, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.py
@@ -0,0 +1,10 @@
+a=int(input())
+b=int(input())
+if a>0 and b>0: # Quardrant 1
+    print(1)
+elif a<0 and b>0: # Quardrant 2
+    print(2)
+elif a<0 and b<0: # Quardrant 3
+    print(3)
+elif a>0 and b<0: # Quardrant 4
+    print(4)
